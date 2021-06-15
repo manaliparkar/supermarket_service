2aba7404ac3e228fc7be13d1cb8933df61c58f14
Manali Parkar <manaliparkar9@gmail.com>
2021-06-15 22:17:12 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -15,4 +15,4 @@ from application import app
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True, port=port, threaded=True)
+    app.run(host='0.0.0.0', debug=True, port=port)
